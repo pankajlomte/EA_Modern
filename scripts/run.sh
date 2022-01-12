51de67475fd6a39eb45a36baca96e37e0b0e1049
Karthik KK <karthik@techgeek.co.in>
2022-01-12 14:19:36 +1300

    Updated workflow

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,7 @@ docker-compose -p "$project" build
 
 docker-compose -p "$project" up -d ea_api ea_webapp db node-docker selenium-hub
 docker-compose -p "$project" up --no-deps ea_test
-exit_code=$(docker inspect -f '{{ .State.ExitCode }}' ${project}_ea_test_1)
+exit_code=$(docker inspect -f '{{ .State.ExitCode }}' ${project}_eatest_1)
 
 if [ $exit_code -eq 0 ]; then
     exit $exit_code
