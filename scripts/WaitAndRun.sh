24fec615d04f6b759c2cb94560ee66001494a9bf
ExecuteAutomation <kartmcad@gmail.com>
2022-01-15 10:47:53 +1300

    Update WaitAndRun.sh

--- scripts/WaitAndRun.sh
+++ scripts/WaitAndRun.sh
@@ -8,9 +8,6 @@ until [ ]; do
    sleep 1
 done
 
-until [ ]; do
-  curl -f "http://selenium-hub:4444/wd/hub/status" && break
-  sleep 10
-done
+while true; do if curl -s -m 5 -o /dev/null http://selenium-hub:4444/wd/hub/status; then echo "OK: $(date)"; else echo "FAIL: $(date)"; fi; sleep 5; done
 
 dotnet test --logger "console;verbosity=detailed"
