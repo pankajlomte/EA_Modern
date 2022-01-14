be47283d2ea5a80144042e57e2d65f3c626c1608
Karthik KK <karthik@techgeek.co.in>
2022-01-15 12:23:48 +1300

    WIP

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,7 @@ docker-compose -p "$project" build
 
 docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub firefox chrome
 docker-compose -p "$project" up --no-deps ea_test
+ls -l
 exit_code=$(docker inspect ${project}_ea_test_1 -f '{{ .State.ExitCode }}')
 
 if [ $exit_code -eq 0 ]; then
