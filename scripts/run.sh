5e261612c87ab8131f4387ccc059bc7fcbb2a9a3
Karthik KK <karthik@techgeek.co.in>
2022-01-16 09:06:22 +1300

    Fixed code

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,8 @@ set -x
 
 project="e2etest"
 
+cd "$(dirname "${0}")/.."
+
 export COMPOSE_HTTP_TIMEOUT=200
 
 docker-compose -p "$project" build
@@ -14,12 +16,11 @@ ls -l
 docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub firefox chrome
 docker-compose -p "$project" up --no-deps ea_test
 
-docker cp ${project}_ea_test:../EATestBDD/bin/Debug/net6.0/TestExecution.json ./reports
-ls -l
+docker cp ea_test:/src/EATestBDD/bin/Debug/net6.0/TestExecution.json ./reports
 echo "TestExecution.json file copied to reports folder"	
 ls -l ./reports
 
-exit_code=$(docker inspect ${project}_ea_test_1 -f '{{ .State.ExitCode }}')
+exit_code=$(docker inspect ea_test -f '{{ .State.ExitCode }}')
 
 if [ $exit_code -eq 0 ]; then
     exit $exit_code
